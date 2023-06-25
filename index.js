0eb351bb256461f344105f512dce6139ce231f89
Isaac <kobonyoisaac998@gmail.com>
2023-06-25 10:39:37 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let main = document.getElementById(`main`)
+main.remove()
+let newHeader = document.getElementById(`victory`)
+newHeader.innerHTML = `Isaac K'Obonyo is the champion`
+append(newHeader)
